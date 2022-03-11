f4c691640835d157856b420c6b6a6ca40a28b5ab
jeongmu.park <jeongmupark@gmail.com>
2022-03-12 01:23:12 +0900

    ✅ 테스트 코드

---
+++ src/components/List.tsx
@@ -0,0 +1,44 @@
+import { collection, onSnapshot } from 'firebase/firestore';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react';
+import { db } from '../../firebase';
+
+interface ReviewType {
+  id: number;
+  title: string;
+  content: string;
+}
+
+export default function List() {
+  const [reviews, setReviews] = useState<any>([]);
+
+  useEffect(() => {
+    onSnapshot(collection(db, 'reviews'), (snapshot) => {
+      setReviews(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }))
+      );
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-blue-800">
+        Hello world!
+      </h1>
+      <div>
+        {reviews.map((review: ReviewType) => (
+          <div key={review.id}>
+            <Link href="/auth">
+              <a>{review.title}</a>
+            </Link>
+            <p>{review.content}</p>
+            <br />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
