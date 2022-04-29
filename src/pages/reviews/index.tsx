7b4b1d4681d9db7fcbbb107349d94f2fefcf6da6
jeongmu.park <jeongmupark@gmail.com>
2022-04-29 10:19:23 +0900

    👽️ msw dummy api 수정

--- src/pages/reviews/index.tsx
+++ src/pages/reviews/index.tsx
@@ -19,7 +19,7 @@ export default function Index() {
   };
 
   const handleClick2 = () => {
-    fetch('/login')
+    fetch('https://jsonplaceholder.typicode.com/posts')
       .then((response) => response.json())
       .then((json) => console.log(JSON.stringify(json)));
   };
