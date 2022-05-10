599d1b0ba5a472fd865cd516f773931b52c071eb
jeongmu.park <jeongmupark@gmail.com>
2022-05-11 01:10:26 +0900

    💄 create 화면 작업중

--- src/pages/create/index.tsx
+++ src/pages/create/index.tsx
@@ -1,5 +1,33 @@
+import { Button, Grid, TextField } from '@mui/material';
 import React from 'react';
 
 export default function Create() {
-  return <div>Create</div>;
+  return (
+    <div>
+      <h2 className="mb-4">Create</h2>
+      <Grid container spacing={2}>
+        <Grid item sm={1} className="my-auto">
+          Label
+        </Grid>
+        <Grid item sm={11}>
+          <TextField className="w-full" />
+        </Grid>
+        <Grid item sm={1} className="my-auto">
+          Label
+        </Grid>
+        <Grid item sm={11}>
+          <TextField className="w-full" />
+        </Grid>
+        <Grid item sm={1} className="my-auto">
+          Label
+        </Grid>
+        <Grid item sm={11}>
+          <TextField className="w-full" />
+        </Grid>
+        <Grid item sm={12} className="text-right">
+          <Button variant="outlined">등록</Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
