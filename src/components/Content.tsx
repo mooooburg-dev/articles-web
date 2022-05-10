ce5c3a360d427abc3ce3d409fd95a47a091366ff
jeongmu.park <jeongmupark@gmail.com>
2022-05-11 00:35:54 +0900

    💄 main 영역 overflow 제거

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -5,9 +5,5 @@ type Props = {
 };
 
 export default function Content({ children }: Props) {
-  return (
-    <main className="relative overflow-auto h-[calc(100vh-140px)] p-4 max-w-6xl mx-auto">
-      {children}
-    </main>
-  );
+  return <main className="relative mx-auto max-w-6xl p-4">{children}</main>;
 }
