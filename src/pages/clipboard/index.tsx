a2fc36ad80e040df8180d3997291dc100bbe329e
mooooburg-dev <jeongmupark@gmail.com>
2022-06-30 19:06:19 +0900

    👽️ clipboard init 구성

--- src/pages/clipboard/index.tsx
+++ src/pages/clipboard/index.tsx
@@ -1,5 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-export default function Saved() {
+const url: string = '';
+
+export default function Clipboard() {
+  const [data, setData] = useState<any>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const getClipboardDatas = () => {
+    fetch(url)
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        setData(json);
+      })
+      .catch((error) => setError(`error: ${error}`));
+  };
+
+  const init = () => {
+    console.log('init');
+  };
+
+  useEffect(init, []);
+
   return <h2>Clipboard</h2>;
 }
