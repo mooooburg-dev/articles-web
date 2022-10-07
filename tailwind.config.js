05c58f5401ac56b302ce079fbabe122c66bacbf2
mooooburg-dev <jeongmupark@gmail.com>
2022-10-07 12:08:55 +0900

    🔧 tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [
+    'postcss-import',
+    'tailwindcss',
+    'autoprefixer',
     function ({ addVariant }) {
       addVariant('child', '& > *');
     },
