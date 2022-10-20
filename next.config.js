98028db11644bb5d3785de3b4c169cc1571adad0
mooooburg-dev <jeongmupark@gmail.com>
2022-10-20 20:06:15 +0900

    🔧 add db config

--- next.config.js
+++ next.config.js
@@ -18,6 +18,13 @@ const nextConfig = {
       },
     ];
   },
+  env: {
+    MYSQL_HOST: process.env.MYSQL_HOST,
+    MYSQL_PORT: process.env.MYSQL_PORT,
+    MYSQL_DATABASE: process.env.MYSQL_DATABASE,
+    MYSQL_USER: process.env.MYSQL_USER,
+    MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+  },
 };
 
 module.exports = nextConfig;
