912456b96360c2053469461cadedfb0130b3e361
mooooburg-dev <jeongmupark@gmail.com>
2022-10-20 15:01:37 +0900

    👽️ added API Route

---
+++ src/pages/api/articles/index.ts
@@ -0,0 +1,11 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  // articles 조회
+  if (req.method === 'GET') {
+  }
+
+  // article 추가
+  if (req.method === 'POST') {
+  }
+}
