eddd492e6c7632adc5eb3d7197bb14986f943c04
mooooburg-dev <jeongmupark@gmail.com>
2022-10-21 18:53:10 +0900

    👽️ update api-util

--- src/helper/api-util.ts
+++ src/helper/api-util.ts
@@ -1,6 +1,9 @@
+import { ArticleType } from 'src/types';
+
 // const url: string = 'https://jsonplaceholder.typicode.com/posts';
 const url: string = 'http://localhost:8090/api/articles';
 
+// article 전체 조회
 export async function getAllDatas() {
   const response = await fetch(url, {
     method: 'GET',
@@ -15,6 +18,18 @@ export async function getClipboardDatas() {
 
   // TODO: 클립보드 데이터 필터 조회 추가
   return allDatas;
+}
+
+// article 단일 수정
+export async function updateArticle(param: ArticleType) {
+  const response = await fetch('http://localhost:8090/api/articles', {
+    method: 'PATCH',
+    body: JSON.stringify(param),
+  });
+
+  const data = await response.json();
+
+  return data;
 }
 
 // 클립보드 저장
