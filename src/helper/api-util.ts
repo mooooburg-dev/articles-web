0d9fde87b169f8934b21179fd70118c78a083fd9
mooooburg-dev <jeongmupark@gmail.com>
2022-10-22 20:35:06 +0900

    🔥 remove code

--- src/helper/api-util.ts
+++ src/helper/api-util.ts
@@ -2,7 +2,6 @@ import { ArticleType } from 'src/types';
 
 // const url: string = 'https://jsonplaceholder.typicode.com/posts';
 const url: string = `${process.env.NEXT_PUBLIC_API_URL}/api/articles`;
-const clipboard_url: string = `${process.env.NEXT_PUBLIC_API_URL}/api/clipboard`;
 
 // article 전체 조회
 export async function getAllDatas() {
