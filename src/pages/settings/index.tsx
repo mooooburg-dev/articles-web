f0c56012c3b4b592127331973c118c6af9c1c08d
mooooburg-dev <jeongmupark@gmail.com>
2023-03-08 16:11:25 +0900

    ✅ firebase 테스트

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { db } from '~/lib/fbase';
+import { addDoc, collection } from '@firebase/firestore';
 
 export default function Settings() {
+  const handleClick = async () => {
+    try {
+      const docRef = await addDoc(collection(db, 'articles'), {
+        title: 'addDocTest',
+      });
+    } catch (e) {
+      console.error('Error adding document: ', e);
+    }
+  };
   return (
     <div>
       <h2>Settings</h2>
+      <div onClick={handleClick}>등록</div>
     </div>
   );
 }
